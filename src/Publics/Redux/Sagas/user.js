8e4b90a79d0c9fb4a73b07c605b9e8f5615a051b
mightydhiyamatra <mightydhiyamatra@yahoo.co.id>
2019-09-29 08:01:12 +0700

    update

--- src/Publics/Redux/Sagas/user.js
+++ src/Publics/Redux/Sagas/user.js
@@ -1,32 +1,3 @@
-// import {takeEvery, call, put} from 'redux-saga/effects';
-// import Axios from 'axios';
-
-// export function* fetchUserWatcher () {
-//   yield takeEvery ('FETCH_USER', fetchUserWorker);
-// }
-
-// export function* fetchUserWorker (action) {
-//   try {
-//     const user = yield call (fetchUserCall);
-//     yield put ({
-//       type: 'SUCCESS',
-//       user,
-//     });
-//   } catch (error) {
-//     yield put ({
-//       type: 'ERROR',
-//       error,
-//     });
-//   }
-// }
-
-// export function fetchUserCall () {
-//   return Axios.get(`https://fortnite-api.theapinetwork.com/weapons/get/`, {
-//     headers: {
-//       Authorization: process.env.REACT_APP_KEY,
-//     },
-//   });
-// }
 import {call, takeEvery, put} from 'redux-saga/effects';
 import Axios from 'axios';
 
